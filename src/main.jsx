fc34ab6b436f6b0e32ab9c61a796ade8ab10351f
AsifurRahman10 <1720105@iub.edu.bd>
2024-11-01 18:17:50 +0600

    navbar and banner added

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./Component/Root/Root.jsx";
+import { Error } from "./Component/Error/Error.jsx";
+import { Home } from "./Component/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
